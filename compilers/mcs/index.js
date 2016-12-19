7809b3c1525ba8e1592ec69eb341454193374f60
Andrew Cain <acain@swin.edu.au>
2016-12-19 12:31:33 +1100

    NEW: Add mcs and dotnet support for windows

---
+++ compilers/mcs/index.js
@@ -0,0 +1,22 @@
+const utils = require('../../utils')
+const home = process.env.HOME
+
+execute = function (argv, callback) {
+  const sklibs = `${home}/.splashkit/lib`
+  const skCSharpCode = `${home}\\.splashkit\\compilers\\mcs\\SplashKit.cs`
+
+  const userArgs = utils.argsToString(argv['original_string'])
+  const mcsArgs = `"${skCSharpCode}"`
+
+  utils.runCommand(`mcs ${userArgs} ${mcsArgs}`, function (err, data) {
+      if (err) {
+          callback(err)
+      } else {
+          callback(null, '🎉  Successfully compiled with mcs 🎉')
+      }
+  })
+}
+
+ module.exports = {
+  execute : execute
+}
