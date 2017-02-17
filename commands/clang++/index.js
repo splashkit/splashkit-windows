5449d38cf30b88b82a054c7a29d5d25091cdee8c
Jake Renzella <j@kerenzella.com>
2017-02-17 15:39:23 +1100

    FIX: Change paths to reflect current directory structure

--- commands/clang++/index.js
+++ commands/clang++/index.js
@@ -3,24 +3,24 @@ const utils = require('../../utils')
 const home = process.env.HOME
 
 execute = function (argv, callback) {
-  const cppBuild = `"${home}/.splashkit/commands/clang++/include"`
-  const includeFolder = `"${home}/.splashkit/include"`
+  const cppBuild = `${home}/.splashkit/commands/clang++/include`
+  const includeFolder = `${home}/.splashkit/include`
   const sklibs = {
-    static: `"${home}/.splashkit/commands/clang++/lib"`,
-    dynamic: `"${home}/.splashkit/lib"`
+    static: `${home}/.splashkit/commands/clang++/lib`,
+    dynamic: `${home}/.splashkit/lib`
   }
 
   let flags
 
   if (process.env.MSYSTEM == 'MINGW32') {
-    flags = `-L"${home}/.splashkit/lib/win32" -static-libstdc++ -static-libgcc -lSplashKitCPP-win32 -lSplashKit -Wl,-Bstatic -lstdc++ -lpthread`
+    flags = `-L${home}/.splashkit/lib/win32 -static-libstdc++ -static-libgcc -lSplashKitCPP-win32 -lSplashKit -Wl,-Bstatic -lstdc++ -lpthread`
 
     var fs = require('fs');
     fs.createReadStream(`${home}/.splashkit/lib/win32/libfreetype-6.dll`).pipe(fs.createWriteStream('libfreetype-6.dll'));
   } else if (process.env.MSYSTEM == 'MINGW64') {
-    flags = `-L"${home}/.splashkit/lib/win64" -static-libstdc++ -static-libgcc -lSplashKitCPP-win64 -lSplashKit -Wl,-Bstatic -lstdc++ -lpthread`
+    flags = `-L${home}/.splashkit/lib/win64 -static-libstdc++ -static-libgcc -lSplashKitCPP-win64 -lSplashKit -Wl,-Bstatic -lstdc++ -lpthread`
   } else {
-    console.log("Can''t determine envioronment.")
+    console.log("Can''t determine environment.")
   }
 
   const userArgs = utils.argsToString(argv['original_string'])
